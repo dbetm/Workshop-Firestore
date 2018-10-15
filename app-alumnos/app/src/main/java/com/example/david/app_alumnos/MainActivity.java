004602fa6a60308748a4a50af7498f255a9a5089
David <davbetm@gmail.com>
2018-10-15 01:45:28 -0500

    Inicio del proyecto

---
+++ app-alumnos/app/src/main/java/com/example/david/app_alumnos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.david.app_alumnos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
