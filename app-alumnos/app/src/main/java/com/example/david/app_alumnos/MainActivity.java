f68a954ee91d1282a6c7fc8e633ad7d14f7ad71b
David <davbetm@gmail.com>
2018-10-15 02:16:04 -0500

    Inicialización de Cloud Firestore

--- app-alumnos/app/src/main/java/com/example/david/app_alumnos/MainActivity.java
+++ app-alumnos/app/src/main/java/com/example/david/app_alumnos/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.david.app_alumnos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+    // Atributos
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setup();
+    }
+
+    public void setup() {
+        db = FirebaseFirestore.getInstance();
     }
 }
